939fda70ed41aecd256a92b0f12c3fb06c6ef299
Aswin M S <aswinms0077@gmail.com>
2025-12-11 22:00:30 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,318 +1,421 @@
-// var a;//Global scope[ReDeclaration,ReInitialization]
-// a=10;
-// var a=20;
-// let b;//Block scope[ReInitialization]
-// b=10;
-// b=20;
-// cons= c=10;  //Block scope
+var a;  // Global Scope [Redeclaration, Reinitialization]
+a=10;
+var a = 20; 
 
-//Example of Data Types in JavaScript
+let b; //Block Scope [Reinitialiation]
+b=10;
+b=20;
 
-var p=20;//number
-var q="Hello";//string
-var bool=true;//boolean
-var r;//undefine
-var s=null;//null
+const c=10; //Block Scope
 
-var symbol = Symbol("li");
+var p = 20; //number
+var q = "Hello"//string
+var bool = true;//boolean
+var r; //undefine
+var s = null; //null
+var big = 1012345678992345678912345678n;
+var symbol =Symbol('li');
 console.log(typeof p);
 console.log(typeof q);
 console.log(typeof bool);
 console.log(typeof r);
 console.log(typeof s);
+console.log(typeof big);
 console.log(typeof symbol);
+var arr = [1,2,3,4,5]; //array
+console.log(arr);
 
-//Array Delcaration with Initialization
-var arr1=[10,20,30,40];
-console.log(arr1);
+var object = { // Object
+    name : "vikraam",
+    age: 24
+}
+console.log(typeof arr);
+console.log(typeof object);
+//Arithametic Operator(+,-,*,/,%)
+var a=10;
+var b="20";
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
 
-var object1={ //Object Declaration with Initialization
-    name:"sri",
-    age:18,
-    city:"Mnp"
-};
-
-//Arithmetic Operators(+,-,*,/,%.**)
+//Relational Operaator(>,<,>=,<=,==,!=,===,!==)
 var a=10;
 var b="10";
-console.log("Addition:",a+b);
-console.log("Subtraction:",b-a);
-console.log("Multiplication:",a*b);
-console.log("Division:",b/a);
-console.log("Modulus:",b%a);  
-console.log("Exponentiation:",a**2); 
+console.log(a>b);
+console.log(a<b);
+console.log(a>=b);
+console.log(a<=b);
+console.log(a==b);
+console.log(a!=b);
+console.log(a===b);
+console.log(a!==b);
 
-//Relational Operators(>,<,>=,<=,==,!=,===,!==)
-console.log("Greater than:",a>b);
-console.log("Less than:",a<b);
-console.log("Greater than or equal to:",a>=b);
-console.log("Less than or equal to:",a<=b);
-console.log("Equal to:",a==b);
-console.log("Not Equal to:",a!=b);  
-console.log("Strict Equal to:",a===b);
-console.log("Strict Not Equal to:",a!==b);
+//Assign Operator (=,+=,-=,*=,/=,%=)
+a = 10;
+console.log(a);
+a+=10;
+console.log(a);
+a-=10;
+console.log(a);
+a*=2;
+console.log(a);
+a/=2;
+console.log(a);
+a%=2;
+console.log(a);
 
-//Assignment Operators(=,+=,-=,*=,/=,%=)
-console.log("Assignment Operators:");
-console.log("a =",a);
-console.log("a += 5:",a+=5);
-console.log("a -= 5:",a-=5);
-console.log("a *= 2:",a*=2);
-console.log("a /= 2:",a/=2);
-console.log("a %= 3:",a%=3);
+//logical Operator (&&,||,!)
+a=true;
+b=false;
+console.log(a&&b);
+console.log(a||b);
+console.log(!a);
 
-//Logical Operators(&&,||,!)
-var x=true;
-var y=false;
-console.log("Logical AND (x && y):",x && y);
-console.log("Logical OR (x || y):",x || y);
-console.log("Logical NOT (!x):",!x);
-
-//Looping Statements
-
-//for(initialization;condition;increment/decrement){
-    //Statement
-
-//}
-for(var i=1;i<=5;i++){
+//for loop
+// for(initialization;Condition;increment/Decrement){
+//     //Statement
+// }
+for(let i=1;i<=10;i++){
     console.log(i);
 }
-//while loop
-//while(condition){
-    //Statement
-//}
-console.log("While Loop:");
-var a=0;
+//While loop
+// while(condition){
+//     //Statement
+// }
+var a =0;
 while(a!==5){
     a++;
-    console.log(a);
+    console.log(a)
 }
+console.log(a);
 
-//do-while loop
-//do{
-    //Statement
-//}while(condition);
+// do...while 
+
+// do{
+//     //statement
+// }while(condition);
+
 a=0;
 do{
     console.log(a);
-
 }while(a===1);
 
-//conditional Statements
-//if condition
-a=5;
+//conditional Statement
+//if
+a =5;
 if(a%2===0){
-    console.log("Even Number");
+    console.log("Even")
 }
+//if..else
+if(a%2===0) console.log("Even");
+else console.log("odd");
 
-//if else condition
-if(a%2===0){
-    console.log("Even Number");
-}
-else{
-    console.log("Odd Number");
-}
-//Else if
-var marks=90;
-if(marks>=90){
-    console.log("O")
-}
-else if(marks>=80){
-    console.log("A+")
-}
-else if(marks>=70){
-    console.log("A")
-}
-else{
-    console.log("B")
-}
+//else..if
+var mark =90;
+if(mark>=90) console.log("O");
+else if(mark>=70) console.log("A");
+else if(mark >=50) console.log("B");
+else console.log("Fail");
 
-//switch statement
-day=1;
+//switch
+day  = 1;
 switch(day){
-    case 1:{
+    case 1:
+    {
         console.log("Sunday");
         break;
+    }
+    case 2:{
+        console.log("Monday");
+        break;
+    }
+    case 3:{
+        console.log("Tuesday");
+        break;
+    }
+    case 4:{
+        console.log("Wednesday");
+        break;
+    }
+    case 5:{
+        console.log("Thursday");
+        break;
+    }
+    case 6:{
+        console.log("Friday");
+        break;
+    }
+    case 7:{
+        console.log("saturday");
+        break;
+    }
+    default:
+        console.log("Invalid Number");
 }
-case 2:{
-    console.log("Monday");
-    break;
-}
-case 3:{
-    console.log("Tuesday");
-    break;
-}
-case 4:{
-    console.log("Wednesday");
-    break;
-}
-case 5:{
-    console.log("Thursday");
-    break;
-}
-case 6:{
-    console.log("Friday");
-    break;
-}
-case 7:{
-    console.log("Saturday");
-    break;
-}
-default:
-    console.log("Invalid day");
-}
-
-//ternary operator
-//syntax
-//condition?value:value
+// terinary Operator (?:)
+// syntax
+// condition?value:value;
 a=10;
-var result = a%2===0 ? "Even" : "Odd";
+var result = (a%2===0) ? "Even" : "odd";
 console.log(result);
 
-marks=55;
-var result = marks>=90 ? "O" : marks>=80 ? "A" : marks>=70 ? "B" : "Fail";
+mark =90;
+var result = (mark>=90) ? "O" : (mark>=70) ? "A" : (mark >=50) ? "B" : "Fail";
 console.log(result);
 
-function add(a,b){
-    console.log(a+b);
-}  add(5+34);
-//Arrow function
+function add(){
+    console.log(10+20)
+}
+add();
 
- var addParam=(a,b)=>{
+//Arrow Function
+var addParam =(a,b)=>{
     console.log(a+b);
 }
+
 addParam(10,20);
-console.log(typeof addParam)
+console.log(typeof addParam);
 
-//spread operator
-const arrA=[1,2,3];
-const arrB=[4,5,6];
-console.log(arrA);
-console.log(arrB);
+// Spread Operator (...)
+var a = [10,20,30];
+var b = [...a,40,50];
+console.log(a);
+console.log(b);
 
-const[myname,age,mark1]=["Kanishka",20,90];
-console.log(myname);
+// Destructuring Operator
+//Array
+var [name,age,marks]= ["vikraam",23,90];
+console.log(name);
 console.log(age);
-console.log(mark1); 
+console.log(marks);
 
-//Object Methods
-[Name,Age,City]="Kanishka",20,"Tiruppur";  
-console.log(Name);
-console.log(Age);
-console.log(City);  
+//Object
+var {name,age,marks} = {
+    name:"vikraam",
+    age:23,
+    marks:90
+}
+console.log(name);
+console.log(age);
+console.log(marks);
 
-//for...in loop
-var arr1=[10,20,30,40];
-for(let i in arr1){
+//for..in (index value)
+var arr = [1,2,3,4,5];
+for(let i in arr){
     console.log(i);
 }
-//for...of loop
-for(let j of arr1){
+//for..of(values)
+for(let j of arr){
     console.log(j);
 }
-//foreach loop
-arr1.forEach((value,index)=>{
-    console.log(value,index);
-});
+
+//forEach
+arr.forEach((val,index) =>{
+    console.log(val,index)
+})
+
+// Class and Object
 class Person{
     name;
     age;
     constructor(name,age){
-        this.name=name;
-        this.age=age;
+        this.name = name;
+        this.age = age;
     }
     display(){
-        console.log("Name:",this.name);
-        console.log("Age:",this.age);
+        console.log(this.name,this.age);
     }
 }
-Person1=new Person("Kanishka",20);
-Person1.display();``
+var p1 = new Person("Vikraam",24);
+p1.display();
 
-//prime number
-let num = 17;
-let prime = true;
+// Prime Number
+var a =5;
+var flag = true;
+if(a<=1) flag = false;
 
-for (let i = 2; i < num; i++) {
-    if (num % i === 0) {
-        prime = false;
-        break;
-    }
-}
-
-if (prime) {
-    console.log("Prime");
-} else {
-    console.log("Not Prime");
-}
-
-//palindrome
-function isPalindrome(str) {
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] !== str[str.length - 1 - i]) {
-            return false;
+else {
+    for(let i=2;i<=a/2;i++){
+        if(a%i===0){
+            flag = false;
+            break;
         }
     }
-    return true;
 }
-console.log(isPalindrome("madam")); 
-console.log(isPalindrome("hello")); 
+console.log(flag===true ? "Prime":"Not Prime");
 
-var a=10;//Global scope
+//Palindrome number
+var a =121;
+b = a;
+var rev =0;
+while(a!==0){
+    var rem = Math.floor(a%10);
+    rev = rev*10 + rem;
+    a=Math.floor(a/10);
+}
+if(rev === b) console.log("Palindrome");
+else console.log("Not Palidrome");
+
+// Scope
+var a = 10; // Global scope
 if(true){
-    var b=20;// var Global scope
+    var b = 20; // var Global scope
     console.log(a);
 }
 console.log(a,b);
 
+let a = 10; // Global scope
+if(true){
+    let b = 20; // let Block scope
+    console.log(a);
+}
+console.log(a,b);
 
-// const a=10;//Global scope
-// if(true){
-//     const b=20;// var Global scope
-//     console.log(a);
-// }
-// console.log(A,B);
+const a = 10; // Global scope
+if(true){
+    const b = 20; // const Block scope
+    console.log(a);
+}
+console.log(a,b);
 
-//Hoisting in JavaScript
-var x;//By JavaScript
-console.log(x); // undefined
-var x = 5;    
+//Hoisting
+console.log(a);
+var a =10;
+console.log(a);
+let a =10;
+console.log(a);
+const a =10;
+add();
+function add(){
+    console.log(10+20);
+}
 
-//CallBack function
-// var demo=()=>{
-//     console.log("Demo function executed");
-// }            
-// var main=(callback)=>{
-//     console.log("Main function executed");
-//     callback();
-// }                
-// main(demo);             
+//for..in(Object)
+var obj = {
+    name:"vikraam",
+    age:24,
+    marks:90
+};
+for(let i in obj){
+    console.log(i,obj[i])
+}
 
-var main=(callback)=>{
-    console.log("Main function executed");
+CallBack Function
+
+var demo = ()=>{
+    console.log("Hello World!!")
+}
+
+var main = (callback)=>{
+    console.log("Main Called");
     callback();
 }
-main(()=>{
-    console.log("Demo function executed");
-});
 
-//calllback function
-var add=(a,b,callback)=>{
-    var result=a+b; 
-    callback(result);   
+main(demo);
+
+var add = (a,b,callback)=>{
+    var result = a+b;
+    callback(result);
 }
+
 add(10,20,(res)=>{
     console.log(res)
-
 });
 
-//SetTimeOut
+//SetTimeout 
 console.log("Hello");
+var Demo=()=>{
 
-var demo = () => {
-    setTimeout(() => {
+    setTimeout(()=>{
         console.log("Processing...");
-    }, 2000);
+    },2000)
 }
+Demo();
 
-demo();
+//Template Literals 
+var age = 24;
+var name = "vikraam"
+var str = `vikraam age is ${age}`
+
+console.log(str);
+
+console.log(`${name} age is ${age}`)
+
+console.log(name+" age is "+age);
+
+//Map
+var arr = [1,2,3,4,5];
+var double = arr.map(num=>num**3);
+console.log(double)
+
+//Filter
+var even = arr.filter((num)=>(num%2===0));
+console.log(even);
+
+//Reducer
+var total = arr.reduce((sum,num)=>(sum+num),0);
+console.log(total);
+
+var evenCount = arr.map(num=>num**3).filter(num=>num%2===0).reduce((sum,num)=>(sum+1),0);
+console.log(evenCount);
+
+var student = [
+    {name:"viky",marks:90},
+    {name:"muthu",marks:80},
+    {name:"raja",marks:88},
+    {name:"sudeep",marks:89},
+];
+//filter
+var studentMark = student.filter(s=>s.marks>88);
+console.log(studentMark);
+//map
+var studentName = student.map(s=>s.name)
+console.log(studentName);
+//reduce
+var totalMarks = student.reduce((sum,s)=>(sum+s.marks),0)
+console.log(totalMarks)
+var avg = totalMarks/student.length;
+console.log(avg)
+
+//promise
+const promise = new Promise((resolve,reject)=>{
+    var success = true;
+    if(success){
+        resolve("Success");
+    }
+    else{
+        reject("Failed");
+    }
+})
+promise.then((msg)=>console.log(msg))
+.catch((err)=>console.log(err))
+
+//promise
+const promise = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("success")
+    },2000)
+})
+promise.then((msg)=>console.log(msg))
+.catch((err)=>console.log(err))
+
+const getData =()=>{
+    return fetch('https://jsonplaceholder.typicode.com/posts')
+}
+getData().then((res)=>res.json())
+.then((data)=>console.log(data))
+.catch((err)=>console.log(err))
+
+
+const getData = async(req,res)=>{
+    try{
+        var res=await fetch('https://jsonplaceholder.typicode.com/posts');
+    var data =await res.json();
+    console.log(data);
+    }catch(err){
+        console.log(err);
+    }
+}
+getData();
